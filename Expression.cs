fe1f241b96ec8709dff566f7ef956765815aca07
Finsfir <50079966+Finsfir@users.noreply.github.com>
2021-06-06 21:35:38 +0700

    Add project files.

---
+++ Expression.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Expression
+    {
+        private int target;
+        public int Target
+        {
+            get => target;
+            set
+            {
+                if (value > 0)
+                    target = value;
+            }
+        }
+
+        public List<string> exprQueue = new List<string>();
+
+        public double Result(string str)
+        {
+            for(int pos = 1; str.Length > pos; pos++)
+            {
+                if (Char.IsDigit(str[pos - 1]) && str[pos] == '(')
+                    str = str.Insert(pos, "*");
+            }
+
+            string forSafe = str;
+            for (int pos = 0; str.Length > pos; pos++)
+            {
+                if (str[pos] == '/' || str[pos] == '*')
+                {
+                    if (Char.IsDigit(str[pos - 1]) && Char.IsDigit(str[pos + 1]))
+                    {
+                        int i = -1;
+                        while (pos + i >= 0 && (Char.IsDigit(str[pos + i]) || (str[pos + i] == ',')))
+                            i--;
+                        str = str.Insert(pos + i + 1, "(");
+                        pos++;
+                        i = 1;
+                        while (str.Length > pos + i && (Char.IsDigit(str[pos + i]) || (str[pos + i] == ',')))
+                            i++;
+                        if (str.Length > pos + i)
+                            str = str.Insert(pos + i, ")");
+                        else
+                            str += ")";
+                    }
+                }
+            }
+            double res = Solve(str, 0);
+
+            exprQueue.Add(forSafe + " = " + res.ToString());
+            return res;
+        }
+
+        private double Solve(string str, int pos)
+        {
+            string a = "";
+            while (str.Length > pos && (Char.IsDigit(str[pos]) || (str[pos] == ',')))
+            {
+                a += str[pos];
+                pos++;
+            }
+            if (str.Length == pos && a == "")
+                return 0;
+            else if (str.Length == pos)
+                return double.Parse(a);
+            switch (str[pos])
+            {
+                case '(':
+                    int i = 0;
+                    int brackets = 1;
+                    while (str.Length > (pos + i) && brackets > 0)
+                    {
+                        i++;
+                        if (str[pos + i] == ')')
+                            brackets--;
+                        if (str[pos + i] == '(')
+                            brackets++;
+                    }
+                    i++;
+                    if (str.Length <= pos + i)
+                        return Solve(str, pos + 1);
+                    else
+                        switch (str[pos + i])
+                        {
+                            case '+':
+                                return Solve(str, pos + 1) + Solve(str, pos + i + 1);
+                            case '-':
+                                return Solve(str, pos + 1) - Solve(str, pos + i + 1);
+                            case '*':
+                                return Solve(str, pos + 1) * Solve(str, pos + i + 1);
+                            case '/':
+                                return Solve(str, pos + 1) / Solve(str, pos + i + 1);
+                            case ')':
+                                return Solve(str, pos + 1);
+                            default:
+                                throw new Exception("Invalid input");
+                        }
+                    break;
+                case ')':
+                    if (a != "")
+                        return double.Parse(a);
+                    else
+                        throw new Exception("Misplaced brackets");
+                case '+':
+                    if (a != "")
+                        return double.Parse(a) + Solve(str, pos + 1);
+                    else
+                        throw new Exception("Misplaced '+'-operation");
+                case '-':
+                    if (a != "")
+                        return double.Parse(a) - Solve(str, pos + 1);
+                    else
+                        throw new Exception("Misplaced '-'-operation");
+                case '*':
+                    if (a != "")
+                        return double.Parse(a) * Solve(str, pos + 1);
+                    else
+                        throw new Exception("Misplaced '*'-operation");
+                case '/':
+                    if (a != "")
+                        return double.Parse(a) / Solve(str, pos + 1);
+                    else
+                        throw new Exception("Misplaced '/'-operation");
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return exprQueue[target];
+        }
+    }
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Calculator
+{
+    public sealed partial class MainPage : Page
+    {
+        public enum Flags
+        {
+            Solved,
+            Inputting
+        }
+        Flags flag = Flags.Inputting;
+        string str = "";
+        Expression exp = new Expression();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки
+            string s = (string)((Button)e.OriginalSource).Content;
+            // Добавляем его в текстовое поле
+            if(s == "=")
+            {
+                try
+                {
+                    textBlock.Text += "=" + exp.Result(str).ToString();
+                    calcHistory.Items.Clear();
+                    foreach(string a in exp.exprQueue)
+                        calcHistory.Items.Add(a);
+                }
+                catch (Exception ex)
+                {
+                    Message("Exception", ex.Message);
+                }
+                flag = Flags.Solved;
+            } else if(s == "CLEAR")
+            {
+                textBlock.Text = "";
+                str = "";
+            }else if (s == "CE")
+            {
+                if(str != null && str.Length > 0)
+                    str = str.Remove(str.Length - 1, 1);
+                textBlock.Text = str;
+                flag = Flags.Inputting;
+            }
+            else
+            {
+                if (Flags.Solved == flag)
+                {
+                    str = "";
+                    textBlock.Text = "";
+                    flag = Flags.Inputting;
+                }
+                textBlock.Text += s;
+                str += s;
+            }
+        }
+
+        private async void Message(string title, string content)
+        {
+            ContentDialog deleteFileDialog = new ContentDialog()
+            {
+                Title = title,
+                Content = content,
+                PrimaryButtonText = "ОК"
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+
+            }
+        }
+    }
+}
